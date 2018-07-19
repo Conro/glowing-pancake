8b24f70ed408144c30dcb895c67f292768900595
Jeriah Sweatt <jeriahsweatt@gmail.com>
2018-07-19 12:53:10 -0700

    Added data keywords

--- src/app/data.ts
+++ src/app/data.ts
@@ -3,12 +3,14 @@ import { Service } from './Models/service.model';
 export const SERVICES: Service[] = [
   { id: 1, name: 'Auto', desc: '', img: 'https://i.imgur.com/KsUiXyI.jpg', keywords: ['car', 'auto'], isUsed: false},
   { id: 2, name: 'Aviation', desc: '', img: 'https://i.imgur.com/IdOqUPz.jpg', keywords: ['plane', 'aviation'], isUsed: false },
-  { id: 3, name: 'Collectibles', desc: '', img: 'https://i.imgur.com/Hj88FIu.jpg', keywords: [''], isUsed: false },
-  { id: 4, name: 'Farm & Ranch', desc: '', img: 'https://i.imgur.com/qhhl0M3.jpg', keywords: [''], isUsed: false },
-  { id: 5, name: 'Mobile Device', desc: '', img: 'https://i.imgur.com/ao4jJDd.jpg', keywords: [''], isUsed: false },
-  { id: 6, name: 'Pet Health', desc: '', img: 'https://i.imgur.com/Y6ssiaU.jpg', keywords: [''], isUsed: false },
-  { id: 7, name: 'Rental Property', desc: '', img: 'https://i.imgur.com/Cgpq64f.jpg', keywords: [''], isUsed: false },
-  { id: 8, name: 'Motorcycles', desc: '', img: 'https://i.imgur.com/9x4SNZE.jpg', keywords: [''], isUsed: false },
-  { id: 9, name: 'Watercraft', desc: '', img: 'https://i.imgur.com/1cRQOb3.jpg', keywords: [''], isUsed: false },
-  { id: 10, name: 'Test', desc: '', img: '', keywords: [''], isUsed: false }
+  { id: 3, name: 'Collectibles', desc: '', img: 'https://i.imgur.com/Hj88FIu.jpg', keywords: ['collection', 'collectibles'], isUsed: false },
+  { id: 4, name: 'Farm & Ranch', desc: '', img: 'https://i.imgur.com/qhhl0M3.jpg', keywords: ['farm', 'ranch','barn','stable'], isUsed: false },
+  { id: 5, name: 'Mobile Device', desc: '', img: 'https://i.imgur.com/ao4jJDd.jpg', keywords: ['cellphone', 'mobile', 'device'], isUsed: false },
+  { id: 6, name: 'Pet Health', desc: '', img: 'https://i.imgur.com/Y6ssiaU.jpg', keywords: ['pet', 'dog', 'cat'], isUsed: false },
+  { id: 7, name: 'Rental Property', desc: '', img: 'https://i.imgur.com/Cgpq64f.jpg', keywords: ['rental', 'rent', 'renter'], isUsed: false },
+  { id: 8, name: 'Motorcycles', desc: '', img: 'https://i.imgur.com/9x4SNZE.jpg', keywords: ['motorcycle'], isUsed: false },
+  { id: 9, name: 'Watercraft', desc: '', img: 'https://i.imgur.com/1cRQOb3.jpg', keywords: ['watercraft', 'boat', 'yacht','jet', 'ski'], isUsed: false },
+  { id: 10, name: 'Life Insurance', desc: '', img: '', keywords: ['life', 'insurance', 'marriage'], isUsed: false },
+  { id: 11, name: 'Home Insurance', desc: '', img: 'https://i.imgur.com/KsUiXyI.jpg', keywords: ['house', 'home', 'homeowner'], isUsed: false},
+  { id: 12, name: 'Florida', desc: '', img: 'https://i.imgur.com/KsUiXyI.jpg', keywords: ['Florida'], isUsed: false},
 ];
